02d342be4451f66513cc258c978b6a402d22397e
Korey Hines <dev.kthines@gmail.com>
2024-03-21 14:04:44 -0700

    cli stuff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "api21a505ac": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "api53022fa0": {
       "ApiId": "string",
       "ApiName": "string",
@@ -7,6 +12,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "multipart1da425f2": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "multipart400397a6": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
